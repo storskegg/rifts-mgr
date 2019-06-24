456e34f489a9ed0739d9a5ece92d845616d1f975
Liam Conrad <liam@storskegg.org>
2019-06-24 00:33:32 -0500

    sooper basic powers bits

---
+++ common/powers.go
@@ -0,0 +1,35 @@
+package common
+
+import "time"
+
+// PowerPointType represents the points used to fuel an ability
+type PowerPointType int
+
+const (
+	// ISP are Inner Strength Points, and fuel psionic powers
+	ISP PowerPointType = 0
+	// PPE are Potential Psychic Energy, and fuel magic powers
+	PPE PowerPointType = 1
+)
+
+// Power represents the most generic common denominator between spells, psionics and effects
+type Power struct {
+	Name           string
+	PowerPointType PowerPointType
+	Cost           int
+	SavingThrow    bool // TODO: Update this type
+	Duration       *time.Duration
+	Damage         Roll
+}
+
+type Spell struct {
+	*Power
+}
+
+type Psionic struct {
+	*Power
+}
+
+type Effect struct {
+	*Power
+}
